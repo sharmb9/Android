f5ac6a1918ef50f13331c617f3158c3d946c6d44
Sharma Bilaval <sharmb9@mcmaster.ca>
2018-07-05 18:27:44 -0400

    Made some changes, still alot of bugs

--- BrainGame/app/src/main/java/com/myappcompany/user/braingame/MainActivity.java
+++ BrainGame/app/src/main/java/com/myappcompany/user/braingame/MainActivity.java
@@ -33,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
     int queCount;
     int correctCount;
 
+    boolean gameOver;
+
+    Button playAgainButton;
+
 
 
     public void start(View view){
@@ -53,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
         //setting up question
         promptView.setText(Integer.toString(a) + "+" + Integer.toString(b));
+        locationOfCorrectAns= randInt.nextInt(4);
 
 
 
@@ -80,32 +85,72 @@ public class MainActivity extends AppCompatActivity {
 
     public void optionClicked(View view){
         //view here is the button clicked
-        queCount++;
+
+        //question count is 10
+
+        while (gameOver==false){
+
+            queCount++;
 
 
-        Log.i("Option", view.getTag().toString());
-        Log.i("Correct option", Integer.toString(locationOfCorrectAns));
+            Log.i("Option", view.getTag().toString());
+            Log.i("Correct option", Integer.toString(locationOfCorrectAns));
 
+            if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAns) )){
+                correctCount++;
+                Log.i("Result", "Correct");
+                messageView.setText("Correct!");
+                scoreView.setText(String.valueOf(correctCount) + "/" + String.valueOf(queCount));
+            }
+            else{
+                messageView.setText("Wrong!");
+                Log.i("Result", "Wrong");
+                scoreView.setText(String.valueOf(correctCount) + "/" + String.valueOf(queCount));
+            }
+
+            if (queCount==10){
+                gameOver=true;
+                playAgainButton.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+    }
+
+    public void playAgain(View view){
+        queCount=0;
+        correctCount=0;
+
+        //setting up random integer for question
         Random randInt= new Random();
+        int a= randInt.nextInt(20); // a and b are some random int between 0 and 20
+        int b= randInt.nextInt(20);
+
+        //setting up question
+        promptView.setText(Integer.toString(a) + "+" + Integer.toString(b));
+
         locationOfCorrectAns= randInt.nextInt(4);
 
-        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAns) )){
-            correctCount++;
-            Log.i("Result", "Correct");
-            messageView.setText("Correct!");
-            scoreView.setText(String.valueOf(correctCount) + "/" + String.valueOf(queCount));
+        //setting up options in a array
+        for (int i=0; i<4; i++){
+            int correctAns= a+b;
+            int randomOption= randInt.nextInt(40); //so that sum can go upto sum of max range, i.e. 20+20
+            if (i==locationOfCorrectAns){
+                optionsArray.add(correctAns); // the correct answer
+            }
+            else {
+                while (randomOption==a+b){
+                    randomOption= randInt.nextInt(40);
+                }
+                optionsArray.add(randomOption);
+            }
         }
-        else{
-            messageView.setText("Wrong!");
-            Log.i("Result", "Wrong");
-            scoreView.setText(String.valueOf(correctCount) + "/" + String.valueOf(queCount));
-        }
 
-
-
-        gameScreen();
-
-
+        //setting up options into buttons
+        option1.setText(Integer.toString((Integer) optionsArray.get(0)));
+        option2.setText(Integer.toString((Integer) optionsArray.get(1)));
+        option3.setText(Integer.toString((Integer) optionsArray.get(2)));
+        option4.setText(Integer.toString((Integer) optionsArray.get(3)));
     }
 
     @Override
@@ -121,10 +166,16 @@ public class MainActivity extends AppCompatActivity {
         messageView = (TextView) findViewById(R.id.messageView);
 
 
+
         option1= (Button) findViewById(R.id.option1);
         option2= (Button) findViewById(R.id.option2);
         option3= (Button) findViewById(R.id.option3);
         option4= (Button) findViewById(R.id.option4);
+
+
+        gameOver= false;
+
+        playAgainButton= (Button) findViewById(R.id.playAgainButton);
 
         queCount=0;
         correctCount=0;
